554853f67346796c70f51c53c4a4b7bac699b934
Jeremy Vassallo <vikingjunior2011@gmail.com>
2018-01-31 14:24:12 +0000

    Added jarring animation to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,38 +1,62 @@
+{
 /*global createCanvas*/
 /*global width*/
 /*global ellipse*/
 /*global height*/
+/*global fill*/
+/*global color*/
+/*global stroke*/
+/*global keyCode*/
+/*global saveCanvas*/
+/*global frameRate*/
+}
 
 var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
-function setup(){
-    createCanvas(480, 600);
-    circleDiameter = width/NUM_CIRCLES;
-    circleRadius = circleDiameter/2;
+function setup() {
+  createCanvas(480, 600);
+
+  frameRate(5);
+
+  circleDiameter = width/NUM_CIRCLES;
+  circleRadius = circleDiameter/2;
+
+  rVal = 255;
+  gVal = 0;
+  bVal = 0;
 }
 
 function draw() {
-    var isShifted = false;
-    
-    var y = 0;
-    while(y <= height){
-        
-  var x;
-  
-  if (isShifted) {
+  var isShifted = false;
+
+  var y = height;
+  while (y >= 0) {
+
+    var x;
+
+    if (isShifted) {
       x = circleRadius;
-  }else {
-      x=0;
-  }
-  
-  while(x <= width) {
-      ellipse(x,y, circleDiameter, circleDiameter);
+    } else {
+      x = 0;
+    }
+
+    while (x <= width) {
+      stroke(color(rVal, gVal, bVal));
+      fill(color(rVal, gVal, bVal));
+      ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
-  }
-  
-      y = y + circleRadius;
-      isShifted = !isShifted;
+    }
+
+    y = y - circleRadius;
+    isShifted = !isShifted;
+
+    rVal = (rVal + 254) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal + 3) % 256;
   }
 }
